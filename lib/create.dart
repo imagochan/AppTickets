5488866a790c811d268b85c7909b6ffc9eb6f995
Julio <julioalfredomachado@gmail.com>
2023-08-02 23:36:12 -0600

    added create form

---
+++ lib/create.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CreateData extends StatefulWidget {
+  const CreateData({super.key});
+
+  @override
+  State<CreateData> createState() => _CreateDataState();
+}
+
+class _CreateDataState extends State<CreateData> {
+  var nameController = TextEditingController();
+  var priceController = TextEditingController();
+  var descController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                hintText: "Name here",
+              ),
+            ),
+            TextField(
+              controller: priceController,
+              decoration: const InputDecoration(hintText: "Price here"),
+            ),
+            TextField(
+              controller: descController,
+              decoration: const InputDecoration(
+                hintText: "Desc here",
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Create Data"))
+          ],
+        ),
+      ),
+    );
+  }
+}
