36392e2d7b0fb3e1ad50644583aebac690dd07df
Julio <julioalfredomachado@gmail.com>
2023-08-04 00:32:43 -0600

    added update screen + started edit logic

---
+++ lib/edit.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:node_tutorials/model/product_model.dart';
+
+class EditScreen extends StatefulWidget {
+  final Product data;
+  const EditScreen({super.key, required this.data});
+
+  @override
+  State<EditScreen> createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  var nameController = TextEditingController();
+  var priceController = TextEditingController();
+  var descController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    nameController.text = widget.data.name.toString();
+    priceController.text = widget.data.price.toString();
+    descController.text = widget.data.desc.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
