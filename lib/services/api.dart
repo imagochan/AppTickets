06b78ac066473594c2e701914a7f73f56fe9e32f
Julio <julioalfredomachado@gmail.com>
2023-08-06 00:41:52 -0600

    removing unused code

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,32 +1,12 @@
 import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
-import 'package:node_tutorials/model/product_model.dart';
 import 'package:node_tutorials/model/ticket_model.dart';
 
 class Api {
-  //static const baseUrl = "http://192.168.0.254/api/";
   static const baseUrl = "http://127.0.0.1:2000/api/";
 
-  static addproduct(Map pdata) async {
-    print(pdata);
-    var url = Uri.parse("${baseUrl}add_product");
-
-    try {
-      final res = await http.post(url, body: pdata);
-
-      if (res.statusCode == 200) {
-        var data = jsonDecode(res.body.toString());
-        print(data);
-      } else {
-        print("failed to get response");
-      }
-    } catch (e) {
-      debugPrint(e.toString()); //foundation.dart
-    }
-  }
-
+  //add ticket
   static addTicket(Map tdata) async {
     print(tdata);
     var url = Uri.parse("${baseUrl}add_ticket");
@@ -42,32 +22,6 @@ class Api {
       }
     } catch (e) {
       debugPrint(e.toString()); //foundation.dart
-    }
-  }
-
-  static getProduct() async {
-    List<Product> products = [];
-
-    var url = Uri.parse("${baseUrl}get_product");
-
-    try {
-      final res = await http.get(url);
-      if (res.statusCode == 200) {
-        var data = jsonDecode(res.body);
-
-        data['products'].forEach(
-          (value) => {
-            products.add(Product(value['pname'], value['pprice'],
-                value['pdesc'], value['id'].toString())),
-          },
-        );
-        return products;
-        //print(data);
-      } else {
-        //
-      }
-    } catch (e) {
-      print(e.toString());
     }
   }
 
@@ -93,7 +47,6 @@ class Api {
                 value['fechaVencimiento'],
                 value['fechaPublicacion'],
                 value['fechaFinPublicacion'],
-                //num.tryParse(value['valorCompra']),
                 value['valorCompra'],
                 value['categoria'])),
           },
@@ -108,21 +61,7 @@ class Api {
     }
   }
 
-  //update put method
-
-  static updateProduct(id, body) async {
-    var url = Uri.parse("${baseUrl}update/$id");
-
-    final res = await http.post(url, body: body);
-    if (res.statusCode == 200) {
-      print(jsonDecode(res.body));
-    } else {
-      print("failed to update data");
-    }
-  }
-
-  //update post ticket
-
+  //update ticket
   static updateTicket(id, body) async {
     var url = Uri.parse("${baseUrl}update_ticket/$id");
 
@@ -134,24 +73,7 @@ class Api {
     }
   }
 
-  // delete method
-
-  static deleteProduct(id) async {
-    var url = Uri.parse("${baseUrl}delete/$id");
-
-    final res = await http.post(url);
-
-    if (res.statusCode == 204) {
-      if (res.body.isNotEmpty) {
-        json.decode(res.body);
-      }
-    } else {
-      print("failed to delete");
-    }
-  }
-
-  // delete ticket api
-
+  //delete ticket
   static deleteTicket(id) async {
     var url = Uri.parse("${baseUrl}delete_ticket/$id");
 
