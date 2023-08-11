60bb118a69f6f7ee72737787b5ef1ce66e26a0c0
Julio <julioalfredomachado@gmail.com>
2023-08-11 14:57:39 -0600

    adding titulo & desc field

--- lib/pantallas/pantalla_crear_ticket_form_cookbook.dart
+++ lib/pantallas/pantalla_crear_ticket_form_cookbook.dart
@@ -26,44 +26,56 @@ class MyCustomFormState extends State<MyCustomForm> {
     return Scaffold(
       body: Form(
         key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              // The validator receives the text that the user has entered.
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.send),
-                hintText: 'Hint Text',
-                helperText: 'Helper Text',
-                counterText: '0 characters',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
-                  if (_formKey.currentState!.validate()) {
-                    // If the form is valid, display a snackbar. In the real world,
-                    // you'd often call a server or save the information in a database.
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Processing Data')),
-                    );
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
                   }
+                  return null;
                 },
-                child: const Text('Submit'),
               ),
-            ),
-          ],
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.send),
+                  hintText: 'Hint Text',
+                  helperText: 'Helper Text',
+                  counterText: '0 characters',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "titulo del ticket"),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "descripcion del ticket"),
+              ),
+              
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    if (_formKey.currentState!.validate()) {
+                      // If the form is valid, display a snackbar. In the real world,
+                      // you'd often call a server or save the information in a database.
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
