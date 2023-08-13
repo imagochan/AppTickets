bbf1dd871743a5db4b6cb841fc30b2edc4fc5e0f
Julio <julioalfredomachado@gmail.com>
2023-08-13 17:36:37 -0600

    adding controller and datepicker

--- lib/pantallas/pantalla_crear_ticket_form_cookbook.dart
+++ lib/pantallas/pantalla_crear_ticket_form_cookbook.dart
@@ -20,6 +20,10 @@ class MyCustomFormState extends State<MyCustomForm> {
   // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
 
+  DateTime selectedDate = DateTime.now();
+
+  var fechaVencimientoController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
@@ -32,15 +36,6 @@ class MyCustomFormState extends State<MyCustomForm> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               TextFormField(
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
                 decoration: const InputDecoration(
                   icon: Icon(Icons.send),
                   hintText: 'Hint Text',
@@ -50,14 +45,23 @@ class MyCustomFormState extends State<MyCustomForm> {
                 ),
               ),
               TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "titulo del ticket"),
+                decoration: const InputDecoration(hintText: "titulo"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "descripcion del ticket"),
+                decoration: const InputDecoration(hintText: "descripcion"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
               ),
-              
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16),
                 child: ElevatedButton(
@@ -74,6 +78,34 @@ class MyCustomFormState extends State<MyCustomForm> {
                   child: const Text('Submit'),
                 ),
               ),
+              TextFormField(
+                decoration:
+                    const InputDecoration(hintText: "fecha de vencimiento"),
+                controller: fechaVencimientoController,
+              ),
+              Text(
+                  "${selectedDate.year} - ${selectedDate.month} - ${selectedDate.day}"),
+              Text(selectedDate.toString()),
+              Text("hola"),
+              Text(DateTime.parse('2023-08-13').toString()),
+              Text(DateUtils.dateOnly(selectedDate).toString()),
+              Text("hola2"),
+              ElevatedButton(
+                  onPressed: () async {
+                    final DateTime? dateTime = await showDatePicker(
+                      context: context,
+                      initialDate: selectedDate,
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(3000),
+                    );
+                    if (dateTime != null) {
+                      setState(() {
+                        selectedDate = dateTime;
+                        fechaVencimientoController.text = dateTime.toString();
+                      });
+                    }
+                  },
+                  child: const Text("Choose Date"))
             ],
           ),
         ),
