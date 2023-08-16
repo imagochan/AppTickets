eb845c80c462b7c2a87327fa800ae8fd1223f78d
Julio <julioalfredomachado@gmail.com>
2023-08-16 08:57:55 -0600

    removed nulls from model

--- lib/modelos/modelo_ticket.dart
+++ lib/modelos/modelo_ticket.dart
@@ -1,15 +1,15 @@
 //Modelo de un ticket a utilizar en la App
 
 class Ticket {
-  final String? id;
-  final String? titulo;
-  final String? descripcion;
-  final DateTime? fechaVencimiento; //format to datetime before using
-  final DateTime? fechaPublicacion;
-  final DateTime? fechaFinPublicacion;
-  final num? valorCompra;
+  final String id;
+  final String titulo;
+  final String descripcion;
+  final DateTime fechaVencimiento; //format to datetime before using
+  final DateTime fechaPublicacion;
+  final DateTime fechaFinPublicacion;
+  final num valorCompra;
   //final String? imagenTicket;
-  final String? categoria;
+  final String categoria;
 
   Ticket(
       this.id,
@@ -21,9 +21,8 @@ class Ticket {
       this.valorCompra,
       this.categoria);
 
-  factory Ticket.fromJson(Map<String, dynamic> json) => Ticket(id:json["id"],);
+  //factory Ticket.fromJson(Map<String, dynamic> json) => Ticket(id:json["id"],);
 
   List<Ticket> fromJsonToList(List<dynamic> list) =>
-
       List<Ticket>.from(list.map((x) => Ticket.fromJson(x)));
 }
