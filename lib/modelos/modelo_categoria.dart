24129d8ca60faf5fed589f5962650d8077e84b44
Julio <julioalfredomachado@gmail.com>
2023-08-25 09:53:17 -0600

    created categoria model

---
+++ lib/modelos/modelo_categoria.dart
@@ -0,0 +1,29 @@
+//modelo para una categoria
+
+import 'package:flutter/material.dart';
+
+class Categoria {
+  final String categoriaNombre;
+  final String id;
+
+  Categoria(
+    {
+      required this.categoriaNombre,
+      required this.id
+    }
+  );
+
+  factory Categoria.fromJson(Map<String,dynamic> parametro) => Categoria(
+    categoriaNombre: parametro['categoriaNombre'], 
+    id: parametro['id']
+  );
+
+  List<Categoria> fromJsonToList(List<dynamic> list) =>
+  List<Categoria>.from(list.map((e) => Categoria.fromJson(e)));
+
+  factory Categoria.empty() => Categoria(
+    categoriaNombre: '',
+    id: ''
+  );
+
+}
