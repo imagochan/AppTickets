fc63f2745ff6685cbe688c44c045cb71ad645a16
Julio <julioalfredomachado@gmail.com>
2023-08-30 10:10:41 -0600

    adding phone field to form

---
+++ lib/pantallas/login/widget_campo_telefono.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CampoTelefono extends StatelessWidget {
+  const CampoTelefono(
+      {super.key,
+      required this.telefonoController,
+      required this.hintText,
+      required this.labelText});
+
+  final TextEditingController telefonoController;
+  final String hintText;
+  final String labelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: telefonoController,
+      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        hintText: hintText,
+        labelText: labelText,
+        icon: const Icon(Icons.phone),
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
+}
